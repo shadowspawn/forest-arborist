650c8b9bf22a676b2342873e584e30cd7df70fad
John Gee <j.gee@adinstruments.com>
2016-08-08 13:47:53 +1200

    Adopt airbnb style

--- arm.js
+++ arm.js
@@ -1,35 +1,35 @@
 #!/usr/local/bin/node
 
-var program = require('commander');
+const program = require('commander');
 const chalk = require('chalk');
 
 // Command line processing
 
 program
-   .version('0.0.1');
+  .version('0.0.1');
 
 // Extra help
-program.on('--help', function(){
-   console.log("  See also 'arm <command> --help' to read about a specific subcommand.");
-   console.log('');
-   });
+program.on('--help', () => {
+  console.log("  See also 'arm <command> --help' to read about a specific subcommand.");
+  console.log('');
+});
 
 program
-   .command('install')
-   .description('install dependent repositories')
-   .option('-n, --dry-run', 'do not perform actions, just print output')
-   .action(function(){
-      console.log(chalk.red('not implemented yet'));
-      process.exit(0);
-      });
+  .command('install')
+  .description('install dependent repositories')
+  .option('-n, --dry-run', 'do not perform actions, just print output')
+  .action(() => {
+    console.log(chalk.red('not implemented yet'));
+    process.exit(0);
+  });
 
 program
-   .command('init')
-   .description('add placeholder file at root of source tree')
-   .action(function(){
-      console.log(chalk.red('not implemented yet'));
-      process.exit(0);
-      });
+  .command('init')
+  .description('add placeholder file at root of source tree')
+  .action(() => {
+    console.log(chalk.red('not implemented yet'));
+    process.exit(0);
+  });
 
 program.parse(process.argv);
 
@@ -37,14 +37,13 @@ program.parse(process.argv);
 // so into error handling if reach this code.
 
 // Show help if no command specified.
-if (process.argv.length == 2)
-   {
-   program.outputHelp();
-   process.exit(1);
-   }
+if (process.argv.length === 2) {
+  program.outputHelp();
+  process.exit(1);
+}
 
 // Error in the same style as command uses for unknown option
 console.log('');
-console.log("  error: unknown command `" + process.argv[2] + "'");
+console.log('  error: unknown command `${process.argv[2]}\'');
 console.log('');
 process.exit(1);
