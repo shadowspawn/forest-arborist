4424a6417026d8786be4e1503744f0d282b669c4
John Gee <j.gee@adinstruments.com>
2016-08-12 00:34:45 +1200

    Find and read config file

--- arm.js
+++ arm.js
@@ -6,7 +6,7 @@ const fs = require('fs');
 // Reminder: shell still listed as dependency in package.json, remove if not used
 // const shell = require('shelljs');
 
-// const armConfigFilename = '.arm-config.json';
+const armConfigFilename = 'arm.json';
 const armRootFilename = '.arm-root.json';
 
 function terminate(message) {
@@ -59,24 +59,74 @@ function fileExists(filePath) {
 }
 
 
-function printRoot() {
+function readMasterFolderName() {
+  const cwd = process.cwd();
+  const data = fs.readFileSync(armRootFilename);
+  let rootObject;
+  try {
+    rootObject = JSON.parse(data);
+  } catch (err) {
+    terminate(`problem parsing ${cwd}/${armRootFilename}\n${err}`);
+  }
+  if (rootObject.master === undefined) {
+    terminate(`problem parsing: ${cwd}/${armRootFilename}\nmissing field 'master'`);
+  }
+  return rootObject.master;
+}
+
+
+function cdRootFolder() {
+  const startedInMasterFolder = fileExists(armConfigFilename);
+
   let tryParent = true;
   do {
     const cwd = process.cwd();
 
     if (fileExists(armRootFilename)) {
-      console.log(process.cwd());
+      readMasterFolderName(); // Sanity check
       return;
     }
 
-    // NB: chdir('..') from '/' silently does nothing
+    // NB: chdir('..') from '/' silently does nothing,so check we moved
     process.chdir('..');
     tryParent = (cwd !== process.cwd());
   } while (tryParent);
 
-  terminate('Root of source tree not found. (Do you need to call \'arm init\'?)');
+  if (startedInMasterFolder) {
+    terminate('Root of source tree not found. (Do you need to call \'arm init\'?)');
+  } else {
+    terminate('Root of source tree not found. ');
+  }
 }
 
+
+function printRoot() {
+  cdRootFolder();
+  console.log(process.cwd());
+}
+
+
+function readConfig() {
+  cdRootFolder();
+  const masterFolderName = readMasterFolderName();
+  process.chdir(masterFolderName);
+  const cwd = process.cwd();
+
+  let data;
+  try {
+    data = fs.readFileSync(armConfigFilename);
+  } catch (err) {
+    terminate(`problem opening ${cwd}/${armConfigFilename}\n${err}`);
+  }
+  let rootObject;
+  try {
+    rootObject = JSON.parse(data);
+  } catch (err) {
+    terminate(`problem parsing ${cwd}/${armConfigFilename}\n${err}`);
+  }
+
+  console.log('Found and read config file');
+}
 
 //------------------------------------------------------------------------------
 // Command line processing
@@ -95,7 +145,8 @@ program
   .description('install dependent repositories')
   .option('-n, --dry-run', 'do not perform actions, just print output')
   .action(() => {
-    terminate('not implemented yet');
+    readConfig();
+    terminate('not fully implemented yet');
   });
 
 program
