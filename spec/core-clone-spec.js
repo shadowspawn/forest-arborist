b349d65a9da023260414c6fcd6d6c468aabeb948
John Gee <john@ruru.gen.nz>
2017-02-22 22:43:21 +1300

    Add placeholder for clone unit tests

---
+++ spec/core-clone-spec.js
@@ -0,0 +1,29 @@
+'use strict';
+
+// const tmp = require('tmp');
+// // Mine
+// const core = require('../lib/core');
+// const fsX = require('../lib/fsExtra');
+// const util = require('../lib/util');
+// //
+// const cc = require('./core-common');
+
+
+// function quietDoFor(internalOptions, cmd, args) {
+//   // Classic use of mute, suppress output from (our own) module that does not support it!
+//   const unmute = util.recursiveMute();
+//   try {
+//     core.doForEach(internalOptions, cmd, args);
+//     unmute();
+//   } catch (err) {
+//     unmute();
+//     throw err;
+//   }
+// }
+
+
+describe('core clone:', () => {
+  it('test goes here', () => {
+    pending('test goes here');
+  });
+});
