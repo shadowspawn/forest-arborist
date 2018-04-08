871d473a781274d927b0e1544eb5490651ad3a66
John Gee <john@ruru.gen.nz>
2018-04-08 16:55:29 +1200

    proto mock for console.log

--- tests/core-manifest.test.ts
+++ tests/core-manifest.test.ts
@@ -1,19 +1,22 @@
 import tmp = require("tmp");
 // Mine
+import cc = require("./core-common");
 import coreManifest = require("../src/core-manifest");
 
 
-describe("core manifest:", () => {
+describe.skip("core manifest:", () => {
   const startDir = process.cwd();
+  const tempFolder = tmp.dirSync({ unsafeCleanup: true });
+
+  beforeEach(() => {
+    process.chdir(tempFolder.name);
+  });
 
   afterEach(() => {
     process.chdir(startDir);
   });
 
   test("no forest", () => {
-    const tempFolder = tmp.dirSync({ unsafeCleanup: true });
-    process.chdir(tempFolder.name);
-
     expect(() => {
       coreManifest.doManifest({ });
     }).toThrow();
@@ -29,7 +32,21 @@ describe("core manifest:", () => {
     expect(() => {
       coreManifest.doManifest({ delete: true });
     }).toThrow();
- });
+  });
 
+  test("default manifest sibling", () => {
+    cc.makeNestedGitForest();
+
+    const spy = jest.spyOn(global.console, 'log');
+    expect(jest.isMockFunction(console.log)).toBeTruthy();
+    coreManifest.doManifest({ });
+    // .toHaveBeenLastCalledWith()
+    expect(console.log).toHaveBeenCalled();
+    spy.mockReset();
+    spy.mockRestore();
+
+    console.log("After");
+    expect(jest.isMockFunction(console.log)).toBeFalsy();
+  });
 
 });
