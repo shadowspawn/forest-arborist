b78293d02bc56d309e1612e140d8d515e0807791
John Gee <john@ruru.gen.nz>
2019-07-21 23:51:59 +1200

    Revert "Retry low-tech loading of package.json for version"
    This reverts commit 8ea110d2f6f8f4a4b8704049d428177c00619b88.

--- src/command.ts
+++ src/command.ts
@@ -1,5 +1,4 @@
 import * as commander from "commander";
-import * as fsX from "fs-extra";
 import * as path from "path";
 // Mine
 import * as completion from "./completion";
@@ -12,6 +11,9 @@ import * as coreManifest from "./core-manifest";
 import * as corePull from "./core-pull";
 import * as coreSnapshot from "./core-snapshot";
 import * as util from "./util";
+// Trickery to cope with different relative paths for typescipt and javascript
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const myPackage = require("dummy_for_node_modules/../../package.json");
 
 
 function doStatus() {
@@ -58,8 +60,6 @@ export type Command = commander.Command;
 
 export function makeProgram(): Command {
   const program = new commander.Command();
-
-  const myPackage = fsX.readJsonSync(path.join(__dirname, "..", "..", "package.json"));
 
   program
     .version(myPackage.version)
