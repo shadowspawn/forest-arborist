58be1a656420e61abd1e962102b52c5c939fff22
John Gee <john@ruru.gen.nz>
2022-01-31 19:32:37 +1300

    Allow setting exitCode

--- src/cli.ts
+++ src/cli.ts
@@ -2,7 +2,7 @@
 
 // Mine
 import * as command from "./command";
-import * as process from 'process';
+// import * as process from 'process'; -- use global so can modify exitCode
 import * as util from "./util";
 
 
