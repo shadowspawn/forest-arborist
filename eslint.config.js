535a7546e98f0d57ccde2df1a81a90c05123cd54
John Gee <john@ruru.gen.nz>
2024-06-22 16:12:16 +1200

    Add prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,15 @@
 const eslint = require('@eslint/js');
 const jest = require('eslint-plugin-jest');
 const tseslint = require('typescript-eslint');
-// const prettier = require('eslint-config-prettier');
+const prettier = require('eslint-config-prettier');
 
 module.exports = tseslint.config(
   {
-    ignores: ['**/dist/**', 'eslint.config.js'],
+    ignores: ['**/dist/**', 'eslint.config.js', '.prettierrc.js'],
   },
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
+  prettier,
   {
     files: ['test/**'],
     ...jest.configs['flat/recommended'],
